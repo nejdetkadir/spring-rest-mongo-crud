d9318f1d94650d660a024858952caf722488b5e0
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-08 03:16:11 +0300

    Initial spring boot project with spring initializr

---
+++ src/main/java/com/nejdetkadirbektas/springrestmongocrud/SpringRestMongoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.nejdetkadirbektas.springrestmongocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMongoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestMongoCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nejdetkadirbektas/springrestmongocrud/SpringRestMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nejdetkadirbektas.springrestmongocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMongoCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
